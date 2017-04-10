5f7ffa8b5edc2ac13e68fa47ab3b23952ac13ac3
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-04-10 00:25:44 -0700

    On link click, plays animation, fades out #curtain, sets #curtain display to none

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	var enterButton = document.getElementById('enter');
+
+	var playFlameAnimation = function() {
+		document.getElementById('flame-drawing').style.display = 'block';
+	};
+
+	var openCurtain = function() {
+		document.getElementById('curtain').className += 'hidden';
+	}
+
+	var destroyCurtain = function() {
+		document.getElementById('curtain').style.display = 'none';
+	}
+	
 	enterButton.addEventListener('click', function(event) {
 		event.preventDefault(); 
-		
-		var flameDrawing = document.getElementById('flame-drawing');
-		flameDrawing.style.display = 'block';
+
+		playFlameAnimation();
+		setTimeout(openCurtain, 1000);
+		setTimeout(destroyCurtain, 3000);
 	});
+
 });
