093858d814c4219a374972cc0e53699da3ef0628
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-04-11 22:18:34 -0700

    Adds nav link scrolling functionality

--- resources/js/scrolling-and-waypoints.js
+++ resources/js/scrolling-and-waypoints.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	// SCROLLING
-	$('.js--scroll-to-contact').click(function() {
-		$('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
-	});
+  $('.js--scroll-to-about').click(function() {
+    $('html, body').animate({scrollTop:$('.js--section-about').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-contact').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
+  });
 
   // $(function() {
   //   $('a[href*=#]:not([href=#])').click(function() {
