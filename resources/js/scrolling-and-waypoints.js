ac2626775c9b471e011ed217d4e7b96a26f15907
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-04-12 23:34:11 -0700

    Fixes navigation scroll offset

--- resources/js/scrolling-and-waypoints.js
+++ resources/js/scrolling-and-waypoints.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 	// SCROLLING
   $('.js--scroll-to-portfolio').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-portfolio').offset().top}, 1000);
+    $('html, body').animate({scrollTop: $('.js--section-portfolio').offset().top + 300}, 1000);
   });
 
   $('.js--scroll-to-about').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-about').offset().top}, 1000);
+    $('html, body').animate({scrollTop: $('.js--section-about').offset().top + 300}, 1000);
   });
 
   $('.js--scroll-to-contact').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
+    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top + 300}, 1000);
   });
 
   // Waypoints
